be02d56a54b0dd0d2d1550689f401288094637fd
Roberto Piccini <roberto.piccini@gmail.com>
2019-09-07 18:25:24 +0200

    refactoring: prepend modules

--- init.rb
+++ init.rb
@@ -1,14 +1,14 @@
 require 'redmine_author_filter/project_authors_patch'
 
 unless IssueQuery.included_modules.include? RedmineAuthorFilter::ProjectAuthorsPatch
-  IssueQuery.send(:include, RedmineAuthorFilter::ProjectAuthorsPatch)
+  IssueQuery.prepend(RedmineAuthorFilter::ProjectAuthorsPatch)
 end
 
 Redmine::Plugin.register :redmine_author_filter do
   name 'Redmine Author Filter plugin'
   author 'Roberto Piccini'
   description "it allows issues' filtering using all authors of the project's issues, not only the members of the project (http://www.redmine.org/issues/1875)"
-  version '2.0.0'
+  version '2.0.1'
   url 'https://github.com/piccio/redmine_author_filter'
   author_url 'https://github.com/piccio'
 end
--- lib/redmine_author_filter/project_authors_patch.rb
+++ lib/redmine_author_filter/project_authors_patch.rb
@@ -1,31 +1,22 @@
 module RedmineAuthorFilter
   module ProjectAuthorsPatch
-    def self.included(base) # :nodoc:
-      base.send(:include, InstanceMethods)
 
-      base.class_eval do
-        alias_method_chain :initialize_available_filters, :all_project_authors
-      end
-    end
+    private
 
-    module InstanceMethods
-      private
+    def initialize_available_filters
+      super
 
-      def initialize_available_filters_with_all_project_authors
-        initialize_available_filters_without_all_project_authors
+      if project
+        author_values = []
+        author_values << ["<< #{l(:label_me)} >>", "me"] if User.current.logged?
 
-        if project
-          author_values = []
-          author_values << ["<< #{l(:label_me)} >>", "me"] if User.current.logged?
+        from_user = []
+        project.issues.group(:author_id).each{|i| from_user << [i.author.name, i.author_id.to_s]}
+        from_user.uniq!
+        from_user.sort!
+        author_values += from_user
 
-          from_user = []
-          project.issues.group(:author_id).each{|i| from_user << [i.author.name, i.author_id.to_s]}
-          from_user.uniq!
-          from_user.sort!
-          author_values += from_user
-
-          add_available_filter('author_id', type: :list, values: author_values) unless author_values.empty?
-        end
+        add_available_filter('author_id', type: :list, values: author_values) unless author_values.empty?
       end
     end
   end
